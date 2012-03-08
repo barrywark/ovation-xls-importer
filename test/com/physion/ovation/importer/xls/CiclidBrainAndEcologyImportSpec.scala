8b22e085b83a79251c8e57b0355d63e9d9bcae6c
Barry Wark <barrywark@gmail.com>
2012-03-08 16:53:20 -0500

    Just setting up the project

---
+++ test/com/physion/ovation/importer/xls/CiclidBrainAndEcologyImportSpec.scala
@@ -0,0 +1,9 @@
+package com.physion.ovation.importer.xls
+
+import org.specs2.SpecificationWithJUnit
+
+class CiclidBrainAndEcologyImportSpec extends SpecificationWithJUnit { def is =
+
+    "The Ciclid Brain and Ecology importer should" ^
+      "import xls" ! failure
+}
--- test/com/physion/ovation/importer/xls/XlsImportSpec.scala
+++ test/com/physion/ovation/importer/xls/XlsImportSpec.scala
@@ -4,13 +4,7 @@ import org.specs2.{SpecificationWithJUnit}
 
 
 class XlsImportSpec extends SpecificationWithJUnit {
-  def is =
-    "The XLS importer should" ^
-      "exist" ! success
-}
-
-class CiclidBrainAndEcologyImpoter extends SpecificationWithJUnit { def is =
-
-  "The Ciclid Brain and Ecology importer should" ^
-    "import xls" ! failure
+    def is =
+        "The XLS importer should" ^
+          "exist" ! success
 }
